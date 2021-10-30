3e7b11f009d417576075e994418cf6a6ee49bac9
Ben Giles <bengiles8090@gmail.com>
2021-10-30 13:48:58 -0600

    actually added lab3b hopefully

---
+++ src/views/Cart.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h1>Your Cart</h1>
+  <div v-if="!cart.length"><h2>Cart Empty</h2></div>
+  <CartList :products="cart" />
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    CartList
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+  methods: {
+  }
+}
+</script>
